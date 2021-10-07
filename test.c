a28696cbf3f3459f248916adcdfb8024e3dfd43f
Mathieu Fourré <mathieu.fourre@epita.fr>
2021-10-07 21:45:17 +0200

    clang-format

--- test.c
+++ test.c
@@ -1,30 +1,48 @@
 #include <criterion/criterion.h>
 
-#define eq_int(x, y)\
-{\
-    int a = x;\
-    int b = y;\
-    cr_assert_eq(a, b, "%d != %d", a, b);\
-}
+#define EQ_INT(x, y)                                                           \
+    {                                                                          \
+        int a = x;                                                             \
+        int b = y;                                                             \
+        cr_assert_eq(a, b, "%d != %d", a, b);                                  \
+    }
 
-#define eq_float(x, y)\
-{\
-    double a = x;\
-    double b = y;\
-    cr_assert_float_eq(a, b, 0.000001, "%f != %f", a, b);\
-}
+#define EQ_FLOAT(x, y)                                                         \
+    {                                                                          \
+        double a = x;                                                          \
+        double b = y;                                                          \
+        cr_assert_float_eq(a, b, 0.000001, "%f != %f", a, b);                  \
+    }
 
-#define eq_str(x, y)\
-{\
-    char *a = x;\
-    char *b = y;\
-    cr_assert_str_eq(a, b, "%s != %s", a, b);\
-}
+#define EQ_STR(x, y)                                                           \
+    {                                                                          \
+        char *a = x;                                                           \
+        char *b = y;                                                           \
+        cr_assert_str_eq(a, b, "%s != %s", a, b);                              \
+    }
 
-#define eq_ptr(x, y)\
-{\
-    int *a = x;\
-    int *b = y;\
-    cr_assert_eq(a, b, "%p != %p", (void*)a, (void*)b);\
-}
+#define EQ_INT_ARR(x, y, len)                                                  \
+    {                                                                          \
+        int *a = x;                                                            \
+        int *b = y;                                                            \
+        char a_str[256];                                                       \
+        char *ap = a_str;                                                      \
+        char b_str[256];                                                       \
+        char *bp = b_str;                                                      \
+        for (unsigned long i = 0; i < len; i++)                                \
+        {                                                                      \
+            ap += sprintf(ap, "%d, ", a[i]);                                   \
+            bp += sprintf(bp, "%d, ", b[i]);                                   \
+        }                                                                      \
+        for (unsigned long i = 0; i < len; i++)                                \
+        {                                                                      \
+            cr_assert_eq(a[i], b[i], "%s != %s", a_str, b_str);                \
+        }                                                                      \
+    }
 
+#define EQ_PTR(x, y)                                                           \
+    {                                                                          \
+        int *a = x;                                                            \
+        int *b = y;                                                            \
+        cr_assert_eq(a, b, "%p != %p", (void *)a, (void *)b);                  \
+    }
